f7712520abcb89b9734a4d4e3e3da0009e607d68
Esha Raj <eshhacb@gmail.com>
2024-12-28 14:56:14 +0530

    Backend done

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { sendMessage } from "../controllers/messageController.js";
+import { sendMessage, getMessage } from "../controllers/messageController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 const router=express.Router();
 
 router.route("/send/:id").post(isAuthenticated,sendMessage);
+router.route("/:id").get(isAuthenticated,getMessage);
 //id contains the id to whom we're sending
 export default router;
