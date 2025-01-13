200b30d6f8d1daa535b1a98f7045fffe6478f0d5
Esha Raj <eshhacb@gmail.com>
2025-01-13 16:31:08 +0530

    websocket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import HomePage from './components/HomePage.jsx'
 import Signup from './components/Signup.jsx'
 import Login from '../src/components/Login.jsx'
+import { useSelector } from 'react-redux'
+import io from "socket.io-client"
 
 const router=createBrowserRouter([
   {
@@ -21,7 +23,21 @@ const router=createBrowserRouter([
 ])
 
  function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket]=useState(null)
+  const {authUser}=useSelector(store=>store.user);
+
+  useEffect(()=>{
+    if(authUser){
+      const socket=io('http://localhost:8080',{
+        transports: ['websocket'],
+      });
+    
+
+      setSocket(socket);
+
+     
+    }
+  },[authUser])
 
   return (
     <>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +8,15 @@ export default defineConfig({
     postcss: {
       plugins: [tailwindcss()],
     },
-  }
-})
+  },
+  server: {
+    proxy: {
+      // Proxy WebSocket requests to your backend server
+      '/socket.io': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        ws: true, // Enable WebSocket support
+      },
+    },
+  },
+});
