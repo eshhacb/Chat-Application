c02ea758676f093fb373b518b10bbd67e2de603d
Esha Raj <eshhacb@gmail.com>
2025-01-14 15:21:37 +0530

    Store persisted

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -5,19 +5,18 @@ import { useDispatch, useSelector } from "react-redux";
 import {setSelectedUser} from '../redux/userSlice';
 
 const MessageContainer = () => {
-  const {selectedUser, authUser}=useSelector(store=>store.user);
+  const {selectedUser, authUser, onlineUsers}=useSelector(store=>store.user);
   const dispatch=useDispatch();
 
-  useEffect(()=>{
-    return ()=>dispatch(setSelectedUser(null));
-  },[]);
+  const isOnline = onlineUsers?.includes(selectedUser?._id);
+
   return (
     <>
     {
       selectedUser !== null ? (
         <div className='md:min-w-[550px] flex flex-col'>
           <div className='flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2'>
-            <div className="avatar online">
+            <div className={`avatar ${isOnline ? 'online' : ''}`}>
               <div className="w-12 rounded-full">
                 <img src={selectedUser?.profilePhoto} alt="user-profile" />
               </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import {Toaster} from 'react-hot-toast'
 import {Provider} from 'react-redux';
-import store from './redux/store.js'
+import store from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist'
+
+
+let persistor = persistStore(store);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-    <App />
-    <Toaster/>
-    </Provider>  
+     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider> 
   </StrictMode>,
 )
 
