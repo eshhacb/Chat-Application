6594689a29b26bf8f13498ce17d45b4c14b9e5d5
Esha Raj <eshhacb@gmail.com>
2025-08-24 17:01:40 +0530

    frontend url changes

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,44 +1,44 @@
-import {Server} from "socket.io"
-import http from 'http'
-import express from 'express'
+import dotenv from 'dotenv';
+import { Server } from "socket.io";
+import http from 'http';
+import express from 'express';
 
+dotenv.config();
 
-const app=express();
+const app = express();
+const server = http.createServer(app);
 
-const server=http.createServer(app);
-const io=new Server(server,{
-    path:'/socket',
-    wssEngine:['ws'],
-    transports:['websocket','polling'],
-    cors:{
-        origin:['http://localhost:5173'],
-        methods:['GET','POST'],
+const io = new Server(server, {
+    path: '/socket',
+    wssEngine: ['ws'],
+    transports: ['websocket', 'polling'],
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        methods: ['GET', 'POST'],
     },
 });
 
-export const getReceiverSocketId=(receiverId)=>{
+export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
-}
+};
 
-const userSocketMap={}; //{userId->socketId}
+const userSocketMap = {}; // {userId -> socketId}
 
-//connection is predefined even in socke.io
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('user connected', socket.id);
 
-    const userId=socket.handshake.query.userId
-    if(userId!== 'undefined'){
-        userSocketMap[userId]=socket.id;
+    const userId = socket.handshake.query.userId;
+    if (userId !== 'undefined') {
+        userSocketMap[userId] = socket.id;
     }
 
-    io.emit('getOnlineUsers',Object.keys(userSocketMap));
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
 
-    socket.on('disconnect',()=>{
+    socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
         delete userSocketMap[userId];
-        io.emit('getOnlineUsers',Object.keys(userSocketMap));
-        //broadcasted again
-    })
-})
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    });
+});
 
-export {app,io,server};
+export { app, io, server };
